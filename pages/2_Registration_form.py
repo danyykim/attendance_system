8a1e95748df4b0fb33c2ec0ab9c4c589fffe5895
danyykim <daniealhkm@gmail.com>
2024-09-20 17:00:53 +0800

    update

--- pages/2_Registration_form.py
+++ pages/2_Registration_form.py
@@ -16,9 +16,8 @@ registration_form = face_rec.RegistrationForm()
 # form
 person_name = st.text_input(label='Name',placeholder='First & Last Name')
 role = st.selectbox(label='Select your Role',options=('Student',
-                                                      'Teacher',
-                                                      'Test'), index=0)
-ic_number = st.number_input(label='IC Number', placeholder='Enter your 12-digit IC Number')
+                                                      'Teacher'))
+ic_number = st.text_input(label='IC Number', placeholder='Enter your 12-digit IC Number')
 
 # Validate IC number
 if len(str(ic_number)) != 12:
