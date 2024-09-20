1dcbe7950b58d2300f7528dba81fc8fadd22a694
danyykim <daniealhkm@gmail.com>
2024-09-20 11:07:27 +0800

    attendance_app

--- Home.py
+++ Home.py
@@ -1,13 +1,13 @@
 import streamlit as st
+import streamlit_authenticator as stauth
 
 
-
-st.set_page_config(page_title='Face Recognition System',layout='wide')
+st.set_page_config(page_title='Attendance System',layout='wide')
 
 st.header('Attendance System using Face Recognition')
 
 with st.spinner("Loading Models and Connecting to Redis db ..."):
     import face_rec
-    
-st.success('Model loaded sucesfully')
-st.success('Redis db sucessfully connected')
+        
+    st.success('Model loaded sucesfully')
+    st.success('Redis db sucessfully connected')
--- face_rec.py
+++ face_rec.py
@@ -1,7 +1,6 @@
 import numpy as np
 import pandas as pd
 import cv2
-
 import redis
 
 # insight face
