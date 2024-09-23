26b589e96e21afc73178691b7594ad2474ae1c16
danyykim <daniealhkm@gmail.com>
2024-09-23 10:42:08 +0800

    update

--- Home.py
+++ Home.py
@@ -1,12 +1,41 @@
+import pickle
+from pathlib import Path
+
 import streamlit as st
+import streamlit_authenticator as stauth
 
 
 st.set_page_config(page_title='Attendance System',layout='wide')
 
-st.header('Attendance System using Face Recognition')
+#user-authentication
+names = ["Danieal"]
+usernames = ["nyel"]
 
-with st.spinner("Loading Models and Connecting to Redis db ..."):
-    import face_rec
-        
-    st.success('Model loaded sucesfully')
-    st.success('Redis db sucessfully connected')
+#load hash password
+file_path = Path(__file__).parent / "hashed_pw.pkl"
+with file_path.open("rb") as file:
+    hashed_passwords = pickle.load(file)
+    
+authenticator = stauth.Authenticate(names, usernames, hashed_passwords, "home_dashboard", "abcdef", cookie_expiry_days=30)
+
+name, authentication_status, username = authenticator.login("Login", "main")
+
+if authentication_status == False:
+  st.error("Username/password is incorrect")
+  
+if authentication_status == None:
+  st.warning("Please enter your username and password")
+
+if authentication_status:
+
+
+    authenticator.logout("Logout", "sidebar")
+    st.sidebar.title(f"Welcome {name}")
+    st.sidebar.header("Home")
+    st.header('Attendance System using Face Recognition')
+
+    with st.spinner("Loading Models and Connecting to Redis db ..."):
+        import face_rec
+            
+        st.success('Model loaded sucesfully')
+        st.success('Redis db sucessfully connected')
