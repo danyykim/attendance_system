c76b56ae0977a2337b7c6868a14d6f0ded1715b4
danyykim <daniealhkm@gmail.com>
2024-09-27 16:28:40 +0800

    update

--- pages/3_Report.py
+++ pages/3_Report.py
@@ -18,7 +18,7 @@ def load_logs(name,end=-1):
     return logs_list
 
 # tabs to show the info
-tab1, tab2 = st.tabs(['Registered Data','Logs'])
+tab1, tab2, tab3 = st.tabs(['Registered Data','Logs','Attendance Report'])
 
 with tab1:
     if st.button('Refresh Data'):
@@ -35,4 +35,34 @@ with tab2:
     if st.button('Refresh Logs'):
         st.write(load_logs(name=name))
         
+with tab3:
+    st.subheader('Attendance Report')
 
+    #load logs into attribute logs_list
+    logs_list = load_logs(name=name)
+    
+    # step -1: convert the logs that in list of bytes into list of string
+    convert_byte_to_string = lambda x: x.decode("utf-8")
+    logs_list_string = list(map(convert_byte_to_string, logs_list))
+    
+    #step 2 - split string by @ and create nested list
+    split_string = lambda x: x.split('@')
+    logs_nested_list = list(map(split_string, logs_list_string))
+    #convert nested list info into dataframe
+    
+    logs_df = pd.DataFrame(logs_nested_list, columns= ['Name','Role','Timestamp'])
+    
+    # step -3 Time based Analysis or Report
+    logs_df["Timestamp"] = pd.to_datetime(logs_df['Timestamp'])
+    logs_df["Date"] = logs_df['Timestamp'].dt.date
+    
+    #step 3.1 Call Intime and Outtime
+    # In time: At which person is first detected in that day(min Timestamp of the date)
+    # Out Time: At which person is last detected in that day(min Timestamp of the date)
+    
+    report_df = logs_df.groupby(by=['Date','Name','Role']).agg(
+        In_time = pd.NamedAgg('Timestamp','min'), #in time
+        Out_time = pd.NamedAgg('Timestamp','max'), # out time
+    ).reset_index()
+    
+    st.dataframe(report_df)
