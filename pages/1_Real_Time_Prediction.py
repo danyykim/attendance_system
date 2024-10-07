2272bebb7d312dc7c011cc84bc31a7e8aea4787d
danyykim <daniealhkm@gmail.com>
2024-10-07 14:06:25 +0800

    update

--- pages/1_Real_Time_Prediction.py
+++ pages/1_Real_Time_Prediction.py
@@ -1,50 +1,51 @@
-import streamlit as st 
+import streamlit as st
 from Home import face_rec
 from streamlit_webrtc import webrtc_streamer
 import av
 import time
 
-# st.set_page_config(page_title='Predictions')
 st.subheader('Real-Time Attendance System')
 
-
-# Retrive the data from Redis Database
-with st.spinner('Retriving Data from Redis DB ...'):    
+# Retrieve the data from Redis Database
+with st.spinner('Retrieving Data from Redis DB ...'):    
     redis_face_db = face_rec.retrive_data(name='academy:register')
-    
-st.success("Data sucessfully retrived from Redis")
 
-# time 
-waitTime = 10 # time in sec
+st.success("Data successfully retrieved from Redis")
+
+# Time settings
+waitTime = 10  # time in sec
 setTime = time.time()
-realtimepred = face_rec.RealTimePred() # real time prediction class
+realtimepred = face_rec.RealTimePred()  # Real-time prediction class
 
-# Real Time Prediction
-# streamlit webrtc
-# callback function
+# Flag to check if logs were saved
+logs_saved = False
+
+# Streamlit webrtc
+# Callback function
 def video_frame_callback(frame):
-    global setTime
+    global setTime, logs_saved
     
-    img = frame.to_ndarray(format="bgr24") # 3 dimension numpy array
-    # operation that you can perform on the array
-    pred_img = realtimepred.face_prediction(img,redis_face_db,
-                                        'facial_features',['Name','Role'],thresh=0.5)
+    img = frame.to_ndarray(format="bgr24")  # 3D numpy array
+    pred_img = realtimepred.face_prediction(img, redis_face_db, 'facial_features', ['Name', 'Role'], thresh=0.5)
     
     timenow = time.time()
     difftime = timenow - setTime
+
     if difftime >= waitTime:
         realtimepred.saveLogs_redis()
-        setTime = time.time() # reset time    
-        
-        print('Save Data to redis database')
-    
+        setTime = time.time()  # Reset time    
+        logs_saved = True  # Set flag to True
+        print('Save Data to Redis database')
 
     return av.VideoFrame.from_ndarray(pred_img, format="bgr24")
 
-
 webrtc_streamer(key="realtimePrediction", video_frame_callback=video_frame_callback,
                 rtc_configuration={
-        "iceServers": [{"urls": ["stun:stun.l.google.com:19302"]}]
-    })
+                    "iceServers": [{"urls": ["stun:stun.l.google.com:19302"]}]
+                })
 
 st.subheader("Prediction Results")
+
+# Display success message if logs were saved
+if logs_saved:
+    st.success("Success: Face successfully scanned and logged!")
