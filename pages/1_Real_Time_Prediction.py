1b7c2850d349e32dec6f03ffd37e5c725a9e0ac1
danyykim <daniealhkm@gmail.com>
2024-10-07 14:30:32 +0800

    update

--- pages/1_Real_Time_Prediction.py
+++ pages/1_Real_Time_Prediction.py
@@ -1,50 +1,59 @@
 import streamlit as st
 from Home import face_rec
-from streamlit_webrtc import webrtc_streamer
-import av
 import time
 
 st.subheader('Real-Time Attendance System')
 
-# Retrieve the data from Redis Database
+# Retrieve data from Redis Database
 with st.spinner('Retrieving Data from Redis DB...'):
     redis_face_db = face_rec.retrive_data(name='academy:register')
-
+    
 st.success("Data successfully retrieved from Redis")
 
-# Variables to track success state
-waitTime = 10 # time in sec
-setTime = time.time()
+# Initialize a class instance for Real-Time Prediction
 realtimepred = face_rec.RealTimePred()
 
-# Add session state for logs_saved flag
+# Set session state to store logs status
 if 'logs_saved' not in st.session_state:
     st.session_state['logs_saved'] = False
 
+# Set time for log checking
+waitTime = 10  # seconds
+setTime = time.time()
+
 # Real Time Prediction
+# streamlit webrtc
+from streamlit_webrtc import webrtc_streamer
+import av
+
 def video_frame_callback(frame):
     global setTime
 
-    img = frame.to_ndarray(format="bgr24")  # 3 dimension numpy array
-    pred_img = realtimepred.face_prediction(img, redis_face_db,
-                                            'facial_features', ['Name', 'Role'], thresh=0.5)
+    img = frame.to_ndarray(format="bgr24")  # 3D numpy array
+    # Apply prediction on the frame
+    pred_img = realtimepred.face_prediction(img, redis_face_db, 'facial_features', ['Name', 'Role'], thresh=0.5)
 
+    # Check for wait time and save logs
     timenow = time.time()
     difftime = timenow - setTime
-
     if difftime >= waitTime:
-        realtimepred.saveLogs_redis()  # Save logs
-        st.session_state['logs_saved'] = True  # Set flag to True after saving logs
-        setTime = time.time()  # Reset time
+        realtimepred.saveLogs_redis()
+        st.session_state['logs_saved'] = True
+        setTime = time.time()  # reset time    
 
     return av.VideoFrame.from_ndarray(pred_img, format="bgr24")
 
-# WebRTC streamer
-webrtc_streamer(key="realtimePrediction", video_frame_callback=video_frame_callback,
-                rtc_configuration={"iceServers": [{"urls": ["stun:stun.l.google.com:19302"]}]})
 
-# Display success message outside of callback
+webrtc_streamer(key="realtimePrediction", video_frame_callback=video_frame_callback,
+                rtc_configuration={
+        "iceServers": [{"urls": ["stun:stun.l.google.com:19302"]}]
+    })
+
+# Prediction Results Header
 st.subheader("Prediction Results")
+
+# Display success message if new logs are saved
 if st.session_state['logs_saved']:
-    st.success("Success: Face successfully scanned and logged!")
-    st.session_state['logs_saved'] = False  # Reset the flag for the next event
+    st.success("Success: New face data has been successfully logged!")
+else:
+    st.warning("No new data found in logs.")
