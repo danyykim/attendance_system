c6f1aa2b02e146d77456246b1f8ee43356cd136a
danyykim <daniealhkm@gmail.com>
2024-10-08 09:07:48 +0800

    update

--- pages/1_Real_Time_Prediction.py
+++ pages/1_Real_Time_Prediction.py
@@ -4,7 +4,19 @@ from streamlit_webrtc import webrtc_streamer
 import av
 import time
 
-# JavaScript to simulate button click after 10 seconds
+# Subheader for the Real-Time Attendance System
+st.subheader('Real-Time Attendance System')
+
+# CSS to hide the button
+st.markdown("""
+    <style>
+    .hidden-button {
+        display: none;
+    }
+    </style>
+""", unsafe_allow_html=True)
+
+# JavaScript to simulate a button click after 10 seconds
 st.markdown("""
     <script>
     function simulateClick() {
@@ -14,27 +26,22 @@ st.markdown("""
     </script>
 """, unsafe_allow_html=True)
 
-# Button to save logs (will be clicked automatically)
-if st.button("Save Logs", key="autoButton"):
+# Hidden button to save logs
+if st.button("Save Logs", key="autoButton", help="This button is hidden and will be clicked automatically.", css_class="hidden-button"):
     st.success("Logs saved automatically after 10 seconds!")
 
-# Subheader
-st.subheader('Real-Time Attendance System')
-
-# Retrieve the data from Redis Database
+# Retrieving Data from Redis Database
 with st.spinner('Retrieving Data from Redis DB...'):
     redis_face_db = face_rec.retrive_data(name='academy:register')
 
 st.success("Data successfully retrieved from Redis")
 
-# Time
+# Time-related setup
 waitTime = 10  # time in sec
 setTime = time.time()
-realtimepred = face_rec.RealTimePred()  # real time prediction class
+realtimepred = face_rec.RealTimePred()  # real-time prediction class
 
-# Real Time Prediction
-# streamlit webrtc
-# callback function
+# Video callback function for real-time prediction
 def video_frame_callback(frame):
     global setTime
 
@@ -46,11 +53,11 @@ def video_frame_callback(frame):
     if difftime >= waitTime:
         realtimepred.saveLogs_redis()
         setTime = time.time()  # reset time
-        print('Save Data to redis database')
+        print('Save Data to Redis database')
 
     return av.VideoFrame.from_ndarray(pred_img, format="bgr24")
 
-# Start the video stream
+# Streamlit WebRTC streamer for real-time video
 webrtc_streamer(key="realtimePrediction", video_frame_callback=video_frame_callback,
                 rtc_configuration={
                     "iceServers": [{"urls": ["stun:stun.l.google.com:19302"]}]
