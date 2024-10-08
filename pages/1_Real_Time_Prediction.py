52f9cdebed7da97bb214e4797e3a6da1c5762d05
danyykim <daniealhkm@gmail.com>
2024-10-08 08:48:15 +0800

    update

--- pages/1_Real_Time_Prediction.py
+++ pages/1_Real_Time_Prediction.py
@@ -14,8 +14,6 @@ with st.spinner('Retriving Data from Redis DB ...'):
     
 st.success("Data sucessfully retrived from Redis")
 
-if 'logs_saved' not in st.session_state:
-    st.session_state.logs_saved = False
 # time 
 waitTime = 10 # time in sec
 setTime = time.time()
@@ -38,7 +36,6 @@ def video_frame_callback(frame):
         realtimepred.saveLogs_redis()
         setTime = time.time() # reset time    
         
-        st.session_state.logs_saved = True
         print('Save Data to redis database')
     
     return av.VideoFrame.from_ndarray(pred_img, format="bgr24")
@@ -49,12 +46,6 @@ webrtc_streamer(key="realtimePrediction", video_frame_callback=video_frame_callb
         "iceServers": [{"urls": ["stun:stun.l.google.com:19302"]}]
     })
 
-
-if st.session_state.logs_saved:
-    with st.expander("Success!", expanded=True):
-        st.write("Logs have been saved successfully.")
-    st.session_state.logs_saved = False  # Reset the flag
-    
 st.subheader("Prediction Results")
 
 
---
+++ pages/Success.py
@@ -0,0 +1,5 @@
+import streamlit as st
+
+st.set_page_config(page_title='Success')
+st.title("Success")
+st.success("Logs have been saved successfully!")
