00b63fd54f0fbb2bab0b572e361a108057fdedeb
danyykim <daniealhkm@gmail.com>
2024-10-09 15:01:19 +0800

    up

--- face_rec.py
+++ face_rec.py
@@ -223,10 +223,10 @@ class RegistrationForm:
         
         return True
 
-def check_ic_exists(self, ic_number):
-        # Check if the IC number is already in the Redis database
-        keys = self.redis_client.keys('*')  # Retrieve all keys
-        for key in keys:
-            if ic_number in key.decode():  # Convert bytes to string for comparison
-                return True
-        return False
+    def check_ic_exists(self, ic_number):
+            # Check if the IC number is already in the Redis database
+            keys = self.redis_client.keys('*')  # Retrieve all keys
+            for key in keys:
+                if ic_number in key.decode():  # Convert bytes to string for comparison
+                    return True
+            return False
