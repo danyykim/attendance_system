a33c5bb23161bdd834f8cb78ca1f1c8832332d9a
danyykim <daniealhkm@gmail.com>
2024-10-10 12:29:09 +0800

    upt

--- pages/1_📸_Scan_Attendance.py
+++ pages/1_📸_Scan_Attendance.py
@@ -35,9 +35,9 @@ if not st.session_state.check_in and not st.session_state.check_out:
 if st.session_state.show_camera:
     
     if st.button('Back'):
-        st.session_state.show_camera = False
         st.session_state.check_in = False
         st.session_state.check_out = False
+        st.session_state.show_camera = False
     # Set up the layout for the camera and status display
     st.subheader('Real-Time Attendance System')
 
