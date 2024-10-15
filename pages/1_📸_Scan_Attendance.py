e5bd566e52a229310bff3c09a40e4453eb014d7c
danyykim <daniealhkm@gmail.com>
2024-10-15 09:20:00 +0800

    up

--- pages/1_📸_Scan_Attendance.py
+++ pages/1_📸_Scan_Attendance.py
@@ -4,18 +4,10 @@ from streamlit_webrtc import webrtc_streamer
 import av
 import time
 import threading
-import redis
 
-hostname = 'redis-10380.c240.us-east-1-3.ec2.redns.redis-cloud.com'
-portnumber = '10380'
-password = '6z4TqpJEaYTnp6dy9renIRjJV3Enlj9i'
-
-r = redis.StrictRedis(host=hostname,
-                      port=portnumber,
-                      password=password)
 # Threading lock for thread-safe access
 lock = threading.Lock()
-success_container = {"success": False, "action": None}  # Shared container
+success_container = {"success": False}  # Shared container
 
 # Set up the layout with buttons
 st.subheader('Attendance System')
@@ -56,7 +48,6 @@ if st.session_state.show_camera:
         st.subheader('Check In')
     elif st.session_state.check_out:
         st.subheader('Check Out')
-        
     # Retrieve data from Redis
     with st.spinner('Retrieving Data from Redis DB ...'):
         redis_face_db = face_rec.retrive_data(name='academy:register')
@@ -76,7 +67,7 @@ if st.session_state.show_camera:
     def video_frame_callback(frame):
         global setTime
         img = frame.to_ndarray(format="bgr24")
-        pred_img, already_checked_in = realtimepred.face_prediction(
+        pred_img = realtimepred.face_prediction(
             img, redis_face_db, 'facial_features', ['Name', 'Role'], thresh=0.5, action=action
         )
         
@@ -87,14 +78,12 @@ if st.session_state.show_camera:
             logged_names, unknown_count = realtimepred.saveLogs_redis()
             setTime = time.time()  # Reset time
             
+            # Thread-safe access
             with lock:
                 success_container["success"] = True
                 success_container["names"] = logged_names
                 success_container["unknown_count"]  = unknown_count
-                success_container["action"] = action
-                success_container["already_checked_in"] = already_checked_in  # Store checked-in status
-              
-                        
+
         return av.VideoFrame.from_ndarray(pred_img, format="bgr24")
 
     ctx = webrtc_streamer(key="realtimePrediction", video_frame_callback=video_frame_callback, rtc_configuration={
@@ -110,19 +99,7 @@ if st.session_state.show_camera:
             if success_container["success"]:
                 names = ', '.join(success_container.get("names", []))  # Join recognized names into a string
                 unknown_count = success_container.get("unknown_count", 0)  # Get unknown person count
-                action_status = success_container.get("action", "Unknown")  # Get action status
-                already_checked_in = success_container.get("already_checked_in", False)  # Get checked-in status
-                
-                if action_status == "Check In":
-                    if already_checked_in:  # User is already checked in
-                        success_message = f"Already Checked In: {names}"
-                    else:
-                        success_message = f"Checked In: {names}"
-                elif action_status == "Check Out":
-                    success_message = f"Checked Out: {names}"
-                else:
-                    success_message = f"Unknown action for {names}"
-               
+                success_message = f"Data has been successfully saved! Names: {names}"
                 if unknown_count > 0:
                     success_message += f" | Unknown Persons Detected: {unknown_count}"
 
