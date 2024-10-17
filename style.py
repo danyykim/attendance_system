af6cc4a0ccc272ebab818a8979a6892e64876598
danyykim <daniealhkm@gmail.com>
2024-10-17 15:40:42 +0800

    upt

--- style.py
+++ style.py
@@ -1,14 +1,17 @@
 import streamlit as st
 # style.py
+# style.py
 def apply_background_style():
     st.markdown(
         """
         <style>
-        .reportview-container {
+        .stApp {
             background: linear-gradient(to bottom, #FF6A00, #FFB700); /* Adjust colors */
             color: white;
+            height: 100vh;
         }
         </style>
         """,
         unsafe_allow_html=True
     )
+
