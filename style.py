341464aabedccc6aee258fd95d099a9e25ca5879
danyykim <daniealhkm@gmail.com>
2024-10-17 16:09:11 +0800

    upt

--- style.py
+++ style.py
@@ -8,7 +8,7 @@ def apply_background_style():
         .stApp {  
             background: rgb(45,189,253);
             background: linear-gradient(0deg, rgba(45,189,253,1) 0%, rgba(228,0,166,1) 100%);
-            color: white;
+            color: black;
             height: 100vh;
         }
         </style>
