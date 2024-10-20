1b813e6ae9951dbf81c0d3f8df5f9d8e1ecd6d68
danyykim <daniealhkm@gmail.com>
2024-10-20 12:25:06 +0800

    up

--- pages/3_📖_Report.py
+++ pages/3_📖_Report.py
@@ -49,7 +49,7 @@ with tab3:
     
     logs_df = pd.DataFrame(logs_nested_list, columns=['Name', 'Role', 'Timestamp', 'Action'])
 
-    logs_df["Timestamp"] = pd.to_datetime(logs_df['Timestamp'], format="%Y-%m-%d %H:%M:%S", errors='coerce')
+    logs_df["Timestamp"] = pd.to_datetime(logs_df['Timestamp'], format=" %H:%M:%S", errors='coerce')
     logs_df["Date"] = logs_df['Timestamp'].dt.date
     
     # Date selection filter
